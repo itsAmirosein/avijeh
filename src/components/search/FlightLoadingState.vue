661284bc5e682f58da23e564b013ff7a2edec27b
amirosein <amirosein.beygi99@gmail.com>
2026-09-20 14:09:23 +0330

    fix: add semantic elements to loadingstate page

--- src/components/search/FlightLoadingState.vue
+++ src/components/search/FlightLoadingState.vue
@@ -1,10 +1,10 @@
 <template>
-  <section
+  <article
     class="flex gap-x-2 mx-auto max-w-7xl space-y-3"
     aria-busy="true"
     aria-live="polite"
   >
-    <div
+    <section
       class="w-1/4 flex flex-col justify-around animate-pulse rounded-card border border-border-default bg-background-surface-default p-5 shadow-card"
     >
       <div
@@ -15,9 +15,9 @@
       >
         <div class="h-28 rounded bg-background-surface-subtle" />
       </div>
-    </div>
+    </section>
 
-    <div class="flex flex-col gap-y-4 w-3/4">
+    <section class="flex flex-col gap-y-4 w-3/4">
       <div
         v-for="index in 5"
         :key="index"
@@ -31,6 +31,6 @@
           <div class="h-12 rounded bg-background-surface-subtle" />
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </article>
 </template>
